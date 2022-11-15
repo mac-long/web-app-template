ec4dd31dd3a79e8c5564c64b62d9e125fab68526
Mac <mac@3sidedcube.com>
2022-11-15 15:18:41 +0000

    ui(root) created basic structure, tailwind setup and base styles.

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+  import "../app.css";
+</script>
+
+<h1 class="text-5xl font-bold">Welcome</h1>
+<slot />
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Inter", "sans-serif"],
+        heading: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
